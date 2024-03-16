f5f520a592bfaabd5c01928976236d8f79e78438
Mohamed Amine <mohamedaminehm07@gmail.com>
2024-03-16 03:46:26 +0100

    creating two files for authentification

---
+++ lib/auth/Emailsignup.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:gym_app/auth/signup.dart';
+
+class EmailSignupScreen extends StatefulWidget {
+  const EmailSignupScreen({super.key});
+
+  @override
+  State<EmailSignupScreen> createState() => _EmailSignupScreenState();
+}
+
+class _EmailSignupScreenState extends State<EmailSignupScreen> {
+  bool? _ischecked = false;
+  bool _secureText = true;
+  TextEditingController _email = TextEditingController();
+  bool _textVerifyEmail = true;
+  String _errorTextEmail = "Email forme non valide";
+
+  TextEditingController _password = TextEditingController();
+  bool _textVerifyPassword = true;
+  bool isValidEmail(String email) {
+    // Regular expression pattern for email validation
+    RegExp regex = RegExp(r'^[\w\.-]+@[a-zA-Z0-9\.-]+\.[a-zA-Z]{2,}$');
+    return regex.hasMatch(email);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "S'inscrire",
+          style: TextStyle(fontWeight: FontWeight.w600),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: SingleChildScrollView(
+          // physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Email",
+                    style: TextStyle(color: Colors.black, fontSize: 17),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "exemple@gmail.com",
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      errorText: _textVerifyEmail ? null : _errorTextEmail,
+                      //errorText: _errorTextEmail,
+                    ),
+                    controller: _email,
+                    onTap: () {
+                      _textVerifyEmail = false;
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        String email = _email.text.toString();
+                        _textVerifyEmail = isValidEmail(email);
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text(
+                    "Mot de passe",
+                    style: TextStyle(color: Colors.black, fontSize: 17),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Minimum 6 caractéres",
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(_secureText
+                            ? CupertinoIcons.eye_fill
+                            : CupertinoIcons.eye_slash_fill),
+                        onPressed: () {
+                          setState(() {
+                            _secureText = !_secureText;
+                          });
+                        },
+                      ),
+                      //errorText: _textVerifyEmail ? _errorTextEmail : null,
+                      errorText: _textVerifyPassword
+                          ? null
+                          : "Mot de passe doit contenir au plus 6 caractéres",
+                    ),
+                    obscureText: _secureText,
+                    controller: _password,
+                    onTap: () {
+                      _textVerifyPassword = false;
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        if (_password.text.toString().length >= 6) {
+                          _textVerifyPassword = true;
+                        } else {
+                          _textVerifyPassword = false;
+                        }
+                      });
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ListTile(
+                contentPadding: const EdgeInsets.only(left: 0, right: 25),
+                leading: Checkbox(
+                    activeColor: Colors.blue,
+                    value: _ischecked,
+                    onChanged: (newBool) {
+                      setState(() {
+                        _ischecked = newBool;
+                      });
+                    }),
+                title: RichText(
+                  text: TextSpan(
+                    style: const TextStyle(fontSize: 17),
+                    children: [
+                      const TextSpan(
+                        text: "J'accepte les ",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      TextSpan(
+                        text: "conditions génerales ",
+                        style: const TextStyle(color: Colors.blue),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            //TODO: page qui contient les conditons generales
+                          },
+                      ),
+                      const TextSpan(
+                        text: "et la ",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      TextSpan(
+                        text: "politique de la confidentialité",
+                        style: TextStyle(color: Colors.blue),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            //TODO: page qui contient les conditons generales
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  fixedSize:
+                      Size.fromWidth(MediaQuery.of(context).size.width * 1),
+                ),
+                onPressed: () {
+                  if (_textVerifyEmail &&
+                      _textVerifyPassword &&
+                      _password.text.isNotEmpty &&
+                      _email.text.isNotEmpty &&
+                      _ischecked == true) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const user()),
+                    );
+                  }
+                },
+                //amine@gmail.com
+                child: const Text(
+                  "Continuer",
+                  style: TextStyle(color: Colors.white, fontSize: 17),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/auth/login.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gym_app/auth/signup.dart';
+import 'package:gym_app/home.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _secureText = true;
+  TextEditingController _email = TextEditingController();
+  bool _textVerifyEmail = true;
+  String _errorTextEmail = "Email forme non valide";
+
+  TextEditingController _password = TextEditingController();
+  bool _textVerifyPassword = true;
+  bool isValidEmail(String email) {
+    // Regular expression pattern for email validation
+    RegExp regex = RegExp(r'^[\w\.-]+@[a-zA-Z0-9\.-]+\.[a-zA-Z]{2,}$');
+    return regex.hasMatch(email);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Se Connecter",
+          style: TextStyle(fontWeight: FontWeight.w600),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: SingleChildScrollView(
+          // physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Email",
+                    style: TextStyle(color: Colors.black, fontSize: 17),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "exemple@gmail.com",
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      errorText: _textVerifyEmail ? null : _errorTextEmail,
+                      //errorText: _errorTextEmail,
+                    ),
+                    controller: _email,
+                    onTap: () {
+                      _textVerifyEmail = false;
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        String email = _email.text.toString();
+                        _textVerifyEmail = isValidEmail(email);
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text(
+                    "Mot de passe",
+                    style: TextStyle(color: Colors.black, fontSize: 17),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Minimum 6 caractéres",
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(_secureText
+                            ? CupertinoIcons.eye_fill
+                            : CupertinoIcons.eye_slash_fill),
+                        onPressed: () {
+                          setState(() {
+                            _secureText = !_secureText;
+                          });
+                        },
+                      ),
+                      //errorText: _textVerifyEmail ? _errorTextEmail : null,
+                      errorText: _textVerifyPassword
+                          ? null
+                          : "Mot de passe doit contenir au plus 6 caractéres",
+                    ),
+                    obscureText: _secureText,
+                    controller: _password,
+                    onTap: () {
+                      _textVerifyPassword = false;
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        if (_password.text.toString().length >= 6) {
+                          _textVerifyPassword = true;
+                        } else {
+                          _textVerifyPassword = false;
+                        }
+                      });
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Mot de passe oublié ?",
+                  style: TextStyle(color: Colors.black, fontSize: 17),
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  fixedSize:
+                      Size.fromWidth(MediaQuery.of(context).size.width * 1),
+                ),
+                onPressed: () {
+                  if (_textVerifyEmail &&
+                      _textVerifyPassword &&
+                      _password.text.isNotEmpty &&
+                      _email.text.isNotEmpty) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                  }
+                },
+                child: const Text(
+                  "Se connecter",
+                  style: TextStyle(color: Colors.white, fontSize: 17),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      // indent: 20,
+                      endIndent: 10,
+
+                      thickness: 1.3,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    "ou",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      indent: 10,
+                      //endIndent: 20,
+
+                      thickness: 1.3,
+                      color: Colors.black,
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              OutlinedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  fixedSize:
+                      Size.fromWidth(MediaQuery.of(context).size.width * 1),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => user()),
+                  );
+                },
+                icon: Image.asset(
+                  "assets/images/google.png",
+                  width: 20,
+                  height: 20,
+                ),
+                label: const Text("Se Connecter avec Google",
+                    style: TextStyle(color: Colors.black)),
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
